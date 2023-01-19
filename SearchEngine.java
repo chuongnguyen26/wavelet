cfc322b33f71c67345e2db09283ab7004d90e212
chuongnguyen26 <chuongn194@gmail.com>
2023-01-19 12:39:51 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> list = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Chuong's number is: %d", num);
+        } 
+        else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } 
+        else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    if(parameters[1].indexOf("app") >= 0){
+                        list.add(parameters[1]);
+                        return String.format("You added: %s", parameters[1]);
+                    }      
+                    return String.format("%s doesn't contain 'app'", parameters[1]);
+                }
+            }
+            else if (url.getPath().contains("/search")){
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    if(parameters[1].equals("app")){
+                        return list.toString();
+                    }
+                }
+            }
+            return "404 Not Found!";
+        }     
+    }  
+}   
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
